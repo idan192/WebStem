67d1fc66a1cae2db8f0256c55107be2d9699678e
idan192 <idan192@gmail.com>
2015-05-18 19:18:08 -0700

    Cleanups

--- js/rs.js
+++ js/rs.js
@@ -17,27 +17,6 @@ $(function() {
 		}
 	});
 
-	if($(window).width() > 640)
-	{
-
-		var headerHeight = $('header').height();
-
-		$(document).scroll(function() {
-			var pos = $(document).scrollTop();
-
-			// $('#contact').css('display', ((pos > headerHeight) ? 'block' : 'none'));
-
-				var parallax = parseInt(pos * -0.3) + 'px';
-				var rgba		 = (pos / headerHeight) * 0.4;
-
-				$('.ryan').css('margin-top', parallax);
-				$('header').css('background', 'rgba(0,0,0,' + rgba);
-
-			
-		});
-	
-	}
-
 	$("#submit").click(function(e) {
 		var fields = {
 			name	:	$("#name"),
